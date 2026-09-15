5d4a0b69904ae6591b4f9754d3546587b79ea538
Sourav Banerjee <sourav.banerjeejobs@gmail.com>
2026-09-15 13:13:56 +0530

    Reset the game (place snake + apple)

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 window.addEventListener("load", function () {
+  // Game data
+  let snakePositions;
+  let applePosition;
+  let score = 0;
+  let contrast = 1;
+  let inputs = [];
+  let gameStarted = false;
+  let hardMode = false;
+
+  // Configuration
+  const speed = 200;
+  let fadeSpeed = 5000;
+  let fadeExponential = 1.024;
+  const contrastIncrease = 0.5;
+  const color = "black";
+
   const width = 15;
   const height = 15;
 
@@ -14,22 +30,47 @@ window.addEventListener("load", function () {
 
     grid.appendChild(tile);
   }
-  const tiles = document.querySelectorAll(".grid, .content, .tile");
+
+  const tiles = document.querySelectorAll(".grid .tile .content");
 
   function setTile(element, overrides = {}) {
     const defaults = {
       width: "100%",
       height: "100%",
       top: "auto",
-      bottom: "auto",
       right: "auto",
+      bottom: "auto",
       left: "auto",
       "background-color": "transparent",
     };
-
     const cssProperties = { ...defaults, ...overrides };
     element.style.cssText = Object.entries(cssProperties)
-      .map(([key, value]) => `${key}: ${value}`)
+      .map(([key, value]) => `${key}: ${value};`)
       .join(" ");
   }
+
+  function resetGame() {
+    snakePositions = [168, 169, 170, 171]; // starting snake
+    applePosition = 100;
+
+    score = 0;
+    contrast = 1;
+    inputs = [];
+
+    // clear all tiles
+    for (const tile of tiles) setTile(tile);
+
+    // draw apple
+    setTile(tiles[applePosition], {
+      "background-color": color,
+      "border-radius": "50%",
+    });
+
+    // draw snake body
+    for (const i of snakePositions) {
+      tiles[i].style.backgroundColor = color;
+    }
+  }
+
+  resetGame(); // call it once at the start
 });
